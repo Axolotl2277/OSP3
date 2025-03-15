c2710a32d338969d8ea56538856313e19ac2e8ea
Axolotl2277 <loganbessinger@gmail.com>
2025-03-15 16:18:00 -0500

    Add files via upload

--- oss.cpp
+++ oss.cpp
@@ -1,26 +1,46 @@
-//This is Project 2 for CMP_SCI-4760 Spring 2025 for Logan Bessinger, 2/27/2025
-//Will take arg -n (for Number) -s (for Simaltainis) -t (for worker self end time) and -i (for time bewteen forking)
+//This is Project 3 for CMP_SCI-4760 Spring 2025 for Logan Bessinger, 3/15/2025
+//Will take arg -n (for Number) -s (for Simaltainis) -t (for worker self end time), -i (for time bewteen forking) and -f (for filename)
 //and create n processes every i milliseconds,but no more that s number at a time, and those files will run for up to t seconds.
 //created processes are named worker.cpp and take 2 argument other than file name and both are ints
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+
 #include <unistd.h>
-#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/wait.h>
 #include <signal.h>
-#include <string>
 #include <unistd.h>
+#include <errno.h>
+
+#include <sys/msg.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <iostream>
-#include <cstdlib>
+#include <sys/wait.h>
 
 #define SHMKEY1 02262025
 #define SHMKEY2 02262026
 #define BUFF_SZ sizeof(int)
 
 using namespace std;
+
+#define PERMS 0644
+typedef struct msgbuffer {
+long mtype;
+char strData[100];
+int intData;
+} msgbuffer;
+
+
+
+msgbuffer buf0;
+  msgbuffer rcvbuf;
+  int msqid;
+  key_t key;
+  
 
 //set up global vars for setting up and killing worker processess and freeing up memory later
 int shmid1 = shmget (SHMKEY1, BUFF_SZ, 0777|IPC_CREAT);
@@ -34,6 +54,7 @@ struct PCB {
 	pid_t pid;
 	int start_Seconds;
 	int start_Nano;
+  int message_Count;
 	};
 struct PCB process_Table[20];
 
@@ -53,7 +74,11 @@ void signal_handler(int sig) {
    shmdt(oss_Seconds);
    shmctl(shmid1,IPC_RMID,NULL);
    shmctl(shmid2,IPC_RMID,NULL);
-   
+   if (msgctl(msqid, IPC_RMID, NULL) == -1)   
+        {
+        perror("msgctl to get rid of queue in parent failed");
+        exit(1);
+        }
   exit(1);
 }
 
@@ -79,11 +104,26 @@ int main( int argc, char** argv)
 	int status = 0;
 	int random_Sec;
   int random_Nano;
-  //control "speed" of process
-  int Nano_Add = 500;
-	
+  int Nano_Add = 250000000; //250 milliseconds to be divided by # of childs later if my math is right
+  string file_name = "defaultFile.txt";
+	system("touch msgq.txt");
+  int total_Messages = 0;
+  int new_Worker_Flag;
+  
+  // get a key for our message queue
+  if ((key = ftok("msgq.txt", 1)) == -1) {
+    perror("ftok");
+    exit(1);
+  }
+  // create our message queue
+  if ((msqid = msgget(key, PERMS | IPC_CREAT)) == -1) {
+    perror("msgget in parent");
+    exit(1);
+  }
+  
+ printf("Parent %d has access to the queue\n",getpid());
 
-	while ((opt = getopt(argc, argv, "n:s:t:i:h")) != -1)
+	while ((opt = getopt(argc, argv, "n:s:t:i:f:h")) != -1)
 	{
 	switch (opt)
 		{
@@ -99,8 +139,11 @@ int main( int argc, char** argv)
 			case 'i':
 				i_Value = atoi(optarg);
 				break;
+      case 'f':
+        file_name = optarg;
+      break;
 			case 'h':
-				printf("Called with (./oss -n x -s y -t z -i a) where x, y, z and a are ints. \n");
+				printf("Called with (./oss -n x -s y -t z -i a -f fileName) where x, y, z and a are ints, and fileName is the name of output file. \n");
 				return 0;
 		}
 	}
@@ -123,6 +166,13 @@ int main( int argc, char** argv)
 	printf("T Value not accepted. Ending process. \n");
 	return -1;
 	}
+ //yeah, so if the name of the file they want to write to is the same as this one... tough luck but I also do not think this is a good file name.
+  if (file_name == "defaultFile.txt"){
+  printf("No change in file name, please select a name or different name for output file.");
+  return -1;
+  }
+ 
+ 
 
 	//cheching here if creating shared memory worked
   //why here? becuase this is where I first tested it in code and did not want to move it 
@@ -140,11 +190,19 @@ int main( int argc, char** argv)
     *oss_Nano_Seconds = 0;             
     *oss_Seconds = 0;
     
-    //Eternal while loop that will be ended with the signal from earlier
-    int wait = 1;
-    while (wait == 1){
-
+    
+    ofstream Working_File(file_name);
+    
+    while (running_Count > 0 || total_Count < n_Value){
+    
+    if (running_Count == 0)
+    {
     *oss_Nano_Seconds = *oss_Nano_Seconds + Nano_Add;
+    }
+    else
+    {
+    *oss_Nano_Seconds = *oss_Nano_Seconds + Nano_Add / running_Count;
+    }
     if (*oss_Nano_Seconds >= 1000000000){
       *oss_Nano_Seconds = *oss_Nano_Seconds - 1000000000;
       *oss_Seconds = *oss_Seconds + 1;
@@ -156,15 +214,20 @@ int main( int argc, char** argv)
     if(n_Value > total_Count && s_Value > running_Count && work_Call_Delta >= i_Value)
       {
       c_pid = fork();
+      new_Worker_Flag = 1;
       if (c_pid == -1)
 		    {
 		    printf("Fork You. Fork Fail. \n");
+            Working_File<<"Process failed to fork. Maybe switch to a spoon. \n\n";
+            Working_File.close();
 		    return 0;
 		    }
       total_Count++;
       running_Count++;
       last_Call_Sec = *oss_Seconds;
       last_Call_Nano = *oss_Nano_Seconds;
+      
+      
       //table setter, possible race condition between table starting value and when the worker process sees itslef starting
       for (int i = 0; i <20; i++)
       {
@@ -180,6 +243,9 @@ int main( int argc, char** argv)
          
    if (c_pid == 0) 
 		{
+   
+   
+   
     srand(*oss_Nano_Seconds);
     random_Sec = rand() % t_Value;
     random_Nano = rand() % 1000000000;
@@ -193,44 +259,128 @@ int main( int argc, char** argv)
 		}
    
    //since I put child pid first and child will become a worker or end before this code, I think it is okay if I do not check pid here.
-   //also wiatpid slows down this process greatly
-    dead_pid = waitpid(-1, &status, WNOHANG);
-    //table cleaner
-    if(dead_pid > 0)
-    {
-    cout<<dead_pid<<"\n";
-    for (int i = 0; i <20; i++)
-      {
-      if (process_Table[i].pid == dead_pid){
-      process_Table[i].occupied = 0;
-	    process_Table[i].pid = 0;
-	    process_Table[i].start_Seconds = 0;
-      process_Table[i].start_Nano = 0;
-      running_Count--;
-      i = 20;     
-      }
-      }
-    }
-    //table outputter
-    if (*oss_Nano_Seconds == 0 || *oss_Nano_Seconds == 500000000)
-    {
-      cout<<"OSS PID:" << getpid() <<" SysClockS: "<<*oss_Seconds<<" SysclockNano: "<< *oss_Nano_Seconds<< "\n";
+   //worker flag check here so every loop does not print table
+   if (new_Worker_Flag == 1){
+   cout<<"OSS PID:" << getpid() <<" SysClockS: "<<*oss_Seconds<<" SysclockNano: "<< *oss_Nano_Seconds<< "\n";
       cout<<"Process table: \n";
-      cout<<"Entry, Occupied, PID, StartS, StartN \n";
+      cout<<"Entry, Occupied, PID, StartS, StartN, MessagesSent \n";
+      Working_File<<"OSS PID:" << getpid() <<" SysClockS: "<<*oss_Seconds<<" SysclockNano: "<< *oss_Nano_Seconds<< "\n";
+      Working_File<<"Process table: \n";
+      Working_File<<"Entry, Occupied, PID, StartS, StartN, MessagesSent \n";
       for (int i = 0; i <20; i++)
       {
       cout<<i<<", ";
       cout<<process_Table[i].occupied<<", ";
 	    cout<<process_Table[i].pid<<", ";
 	    cout<<process_Table[i].start_Seconds<<", ";
-      cout<<process_Table[i].start_Nano<<"\n";      
+      cout<<process_Table[i].start_Nano<<", ";
+      cout<<process_Table[i].message_Count<<"\n";  
+      Working_File<<i<<", ";
+      Working_File<<process_Table[i].occupied<<", ";
+	    Working_File<<process_Table[i].pid<<", ";
+	    Working_File<<process_Table[i].start_Seconds<<", ";
+      Working_File<<process_Table[i].start_Nano<<", ";
+      Working_File<<process_Table[i].message_Count<<"\n"; 
+      new_Worker_Flag = 1;
+      }
+   }
+   
+   
+   //read for every slot in table if slot is occupied send/recive message to/from child
+   for (int i = 0; i < 20; i++)
+   {
+     if (process_Table[i].occupied != 0)
+     {
+     buf0.mtype = process_Table[i].pid;
+     buf0.intData = process_Table[i].pid;
+     strcpy(buf0.strData,"Message to Child ");
+     
+     cout<<"OSS: Sending message to worker "<<i;
+     cout<<"PID "<<process_Table[i].pid<<" at time "<<*oss_Nano_Seconds<<":"<<*oss_Nano_Seconds;
+     Working_File<<"OSS: Sending message to worker "<<i;
+     Working_File<<"PID "<<process_Table[i].pid<<" at time "<<*oss_Nano_Seconds<<":"<<*oss_Nano_Seconds;
+     
+     
+     if (msgsnd(msqid, &buf0, sizeof(msgbuffer)-sizeof(long), 0) == -1) 
+        {
+        perror("msgsnd to child 1 failed\n");
+        exit(1);
+        }
+        
+        cout<<"OSS: Reciving message from worker "<<i;
+     cout<<"PID "<<process_Table[i].pid<<" at time "<<*oss_Nano_Seconds<<":"<<*oss_Nano_Seconds;
+     Working_File<<"PID "<<process_Table[i].pid<<" at time "<<*oss_Nano_Seconds<<":"<<*oss_Nano_Seconds;
+     
+      if (msgrcv(msqid, &rcvbuf,sizeof(msgbuffer), getpid(),0) == -1) 
+        {
+        perror("failed to receive message in parent\n");
+        exit(1);
+        }
+      process_Table[i].message_Count++;
+      total_Messages++;
+      //if child is done clear data from table and output message
+      if (rcvbuf.intData == 0)
+        {
+        cout<<"OSS: Worker "<<i;
+        cout<<"PID "<<process_Table[i].pid<<" is planning to terminate ";
+        Working_File<<"OSS: Worker "<<i;
+        Working_File<<"PID "<<process_Table[i].pid<<" is planning to terminate ";
+        process_Table[i].occupied = 0;
+        process_Table[i].pid = 0;
+	      process_Table[i].start_Seconds = 0;
+        process_Table[i].start_Nano = 0;
+        process_Table[i].message_Count = 0;
+        running_Count--;
+        }
+      
+     }
+   }
+    
+    
+    //table outputter
+    if (*oss_Nano_Seconds == 0 || *oss_Nano_Seconds == 500000000)
+    {
+      cout<<"OSS PID:" << getpid() <<" SysClockS: "<<*oss_Seconds<<" SysclockNano: "<< *oss_Nano_Seconds<< "\n";
+      cout<<"Process table: \n";
+      cout<<"Entry, Occupied, PID, StartS, StartN, MessagesSent \n";
+      Working_File<<"OSS PID:" << getpid() <<" SysClockS: "<<*oss_Seconds<<" SysclockNano: "<< *oss_Nano_Seconds<< "\n";
+      Working_File<<"Process table: \n";
+      Working_File<<"Entry, Occupied, PID, StartS, StartN, MessagesSent \n";
+      for (int i = 0; i <20; i++)
+      {
+      cout<<i<<", ";
+      cout<<process_Table[i].occupied<<", ";
+	    cout<<process_Table[i].pid<<", ";
+	    cout<<process_Table[i].start_Seconds<<", ";
+      cout<<process_Table[i].start_Nano<<", ";
+      cout<<process_Table[i].message_Count<<"\n";  
+      
+      Working_File<<i<<", ";
+      Working_File<<process_Table[i].occupied<<", ";
+	    Working_File<<process_Table[i].pid<<", ";
+	    Working_File<<process_Table[i].start_Seconds<<", ";
+      Working_File<<process_Table[i].start_Nano<<", ";
+      Working_File<<process_Table[i].message_Count<<"\n";   
       }
     }
     
     
 		
 		}
-
-	printf("If seen, an error has happened \n");
+   //close everything and retire for the night
+   if (msgctl(msqid, IPC_RMID, NULL) == -1)   
+        {
+        perror("msgctl to get rid of queue in parent failed");
+        exit(1);
+        }
+        
+        cout<<"Final output: \n Total Count: "<<total_Count << "\nMessages Sent: "<<total_Messages<<"\n\n";
+      Working_File<<"Final output: \nTotal Count: "<<total_Count << "\nMessages Sent: "<<total_Messages<<"\n\n";
+  shmdt(oss_Nano_Seconds);
+  shmdt(oss_Seconds);
+  shmctl(shmid1,IPC_RMID,NULL);
+  shmctl(shmid2,IPC_RMID,NULL);
+  Working_File.close();
+	printf("Ending by no more processes to run. \n");
 	return 0;
 }
--- worker.cpp
+++ worker.cpp
@@ -1,18 +1,29 @@
-//Project 2 for CMP_SCI-4760 Spring 2025 for Logan Bessinger, 2/27/2025
+//Project 3 for CMP_SCI-4760 Spring 2025 for Logan Bessinger, 3/15/2025
 //Outputs a string that is mostly the same execpt for every instance of it
 //Takes two ints as argv, 1st for secounds, 2nd for nanoseconds and after that ammount of time passes, it ends itself
 //outputs its pid, ppid, the current time, and time it ends
-//outputs once while starting, everytime secounds changes, and once when exiting
+//outputs once while starting, everytime oss sends a message, and once when exiting
 //time is not system time but oss time. 
+#include <iostream>
+
 #include <unistd.h>
-#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <signal.h>
+#include <sys/msg.h>
 
+#define PERMS 0644
+typedef struct msgbuffer {
+long mtype;
+char strData[100];
+int intData;
+} msgbuffer;
 
 
 #define SHMKEY1 02262025
@@ -47,6 +58,26 @@ int main(int argc, char** argv)
   int End_Nano = Nano_Sec_To_Kill + start_Time_Nano;
   int sec_Current = start_Time_Sec;
   int sec_Count = 0;
+  int loop_Counter = 0;
+  int done_Check = 1;
+  
+  msgbuffer buf;
+  buf.mtype = 1;
+  int msqid = 0;
+  key_t key;
+  
+  if ((key = ftok("msgq.txt", 1)) == -1) {
+  perror("ftok");
+  exit(1);
+  }
+
+  if ((msqid = msgget(key, PERMS)) == -1) {
+  perror("msgget in child");
+  exit(1);
+  }
+
+  printf("Child %d has access to the queue\n",getpid());
+  
 
   //Logic to ensure end nano is not greater than legal nano value, otherwise while loop later does not work
   if (End_Nano >= 1000000000){
@@ -62,22 +93,49 @@ int main(int argc, char** argv)
   std::cout<< " TermTimeS: " <<End_Sec;
   std::cout<< " TermTimeNano: " <<End_Nano << "\n";
   std::cout<< "--Just Starting \n";
+  
     
+	do {
+   
+   
+    if ( msgrcv(msqid, &buf, sizeof(msgbuffer), getpid(), 0) == -1) {
+    perror("failed to receive message from parent\n");
+    exit(1);
+    }
+
+      if (End_Sec <= *Seconds_Reader) {
+      //testing for both so if nano seconds reset to 0 it will still see to end.
+      if(End_Nano <= *Nano_Seconds_Reader || End_Sec + 1 < *Seconds_Reader)
+    { 
+        done_Check = 0;
+    }
+    }
+	  
     
-    //read as while less than end time, keep working
-	while(End_Sec > *Seconds_Reader || End_Nano > *Nano_Seconds_Reader){
-	  if (sec_Current != *Seconds_Reader){
-        sec_Current = *Seconds_Reader;
-        sec_Count++;
+    if (done_Check == 1){
+        loop_Counter++;
         std::cout<< "Worker PID:" << getpid();
 	      std::cout<< " PPID:" << getppid();
         std::cout<< " Sysclock: " << *Seconds_Reader;
         std::cout<< " SysclockNano: " << *Nano_Seconds_Reader;
         std::cout<< " TermTimeS: " <<End_Sec;
-        std::cout<< " TermTimeNano: " <<End_Nano << "\n";
-        std::cout<< "--"<<sec_Count<<" seconds have passed since starting \n";
+        std::cout<< " TermTimeNano: " <<End_Nano<<" \n";
+        std::cout<< "--"<<loop_Counter<<" interation has passed since it started \n";
     }
-	} 
+    
+    
+    buf.mtype = getppid();
+    buf.intData = done_Check;;
+    strcpy(buf.strData,"Message back to muh parent ");
+    std::cout<<"test\n";
+	  if (msgsnd(msqid,&buf,sizeof(msgbuffer)-sizeof(long),0) == -1) 
+      {
+      perror("msgsnd to parent failed\n");
+      exit(1);
+      }
+    
+
+  }  while(done_Check == 1);
  
     std::cout<< "Worker PID:" << getpid();
 	  std::cout<< " PPID:" << getppid();
@@ -85,7 +143,7 @@ int main(int argc, char** argv)
     std::cout<< " SysclockNano: " << *Nano_Seconds_Reader;
     std::cout<< " TermTimeS: " <<End_Sec;
     std::cout<< " TermTimeNano: " <<End_Nano << "\n";
-    std::cout<< "--Terminating \n";
+    std::cout<< "--Terminating after sending message back to oss after "<<loop_Counter<<" iterations \n";
  
    shmdt(Seconds_Reader);
    shmdt(Nano_Seconds_Reader);
